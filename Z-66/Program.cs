e7cd02a62c186e04786e430a88ff852e14e6bab8
Kapshai <leonid.kapshai@gmail.com>
2022-12-16 01:48:38 +0300

    Рекурсия

---
+++ Z-66/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 66: Задайте значения M и N. 
+// Напишите программу, которая найдёт сумму 
+// натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.WriteLine("Введите 1 число:");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите 2 число:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (m < n)
+    {
+        Console.WriteLine(Sum(m, n));
+    }
+else
+    {
+        Console.WriteLine(Sum(n, m));
+    }
+
+
+int Sum(int start, int end)
+{
+    if (start == end)
+    {
+        return end;
+    }
+    return (end + Sum (start,--end));
+}
---
+++ Z-68/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана
+// с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+Console.WriteLine("(A(m,n) Введите m:");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("(A(m,n) Введите n:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(Sum(m, n));
+
+int Sum(int m, int n)
+{
+    if (m == 0)
+    {
+        return n+1;
+    }
+    if (m == 1)  
+    {       
+        return n+2;
+    }
+    if (m==2)
+    {
+        return 2*n+3;
+    }
+    if (m==3)
+    {
+        int p = 2;
+        for (int i= 2; i<=n+3; i++)
+        {
+            p = p*2;
+        }
+        return p-3;
+    }
+    return (Sum(m,n));
+}
